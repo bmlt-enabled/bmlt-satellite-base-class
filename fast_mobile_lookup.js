f170096ca8b4ab70b8037f660d92e01ff7d34f58
chris@littlegreenviper.com <webservant@magnaws.org>
2014-05-24 14:27:43 -0400

    Adding Danish localization.

--- fast_mobile_lookup.js
+++ fast_mobile_lookup.js
@@ -1,7 +1,7 @@
 /****************************************************************************************//**
 * \file fast_mobile_lookup.js																*
 * \brief Javascript functions for the fast mobile lookup map interface.						*
-*   \version 3.0.22                                                                         *
+*   \version 3.0.23                                                                         *
 * \license Public Domain -No restrictions at all.											*
 ********************************************************************************************/
 
