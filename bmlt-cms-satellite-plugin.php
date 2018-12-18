7a0a081b4ee590ca620b0babc4112ae4b1b3da0b
pjaudiomv <pjaudiomv@gmail.com>
2018-12-18 17:02:23 -0500

    oh deer

--- bmlt-cms-satellite-plugin.php
+++ bmlt-cms-satellite-plugin.php
@@ -634,6 +634,8 @@ abstract class BMLTPlugin
             {
             $BMLTOptions['default_geo_width'] = self::$default_geo_width;
             }
+
+        return $BMLTOptions;
         }
     
     /************************************************************************************//**
