34655618621c9517b777426f4d90b5464c25c28c
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2026-03-19 21:51:24 +0000

    Deploying to gh-pages from @ bmlt-enabled/bmlt-satellite-base-class@358d02accc7d21e05624a23a73ae996236f3fdd1 🚀

--- search/all_4.js
+++ search/all_4.js
@@ -2,9 +2,11 @@ var searchData=
 [
   ['changelist_0',['CHANGELIST',['../index.html#autotoc_md2',1,'']]],
   ['cms_5fdelete_5foption_1',['cms_delete_option',['../d5/dc1/class_b_m_l_t_plugin.html#a9c75c6f237da7458797afc5fdb813ae9',1,'BMLTPlugin\cms_delete_option()'],['../d6/dc9/class_b_m_l_t_u_test_plugin.html#a9c75c6f237da7458797afc5fdb813ae9',1,'BMLTUTestPlugin\cms_delete_option()']]],
-  ['cms_5fget_5foption_2',['cms_get_option',['../d5/dc1/class_b_m_l_t_plugin.html#a812e94daeb7fb59635668fd8e1219ff2',1,'BMLTPlugin\cms_get_option()'],['../d6/dc9/class_b_m_l_t_u_test_plugin.html#a812e94daeb7fb59635668fd8e1219ff2',1,'BMLTUTestPlugin\cms_get_option()']]],
-  ['cms_5fget_5fpage_5fsettings_5fid_3',['cms_get_page_settings_id',['../d5/dc1/class_b_m_l_t_plugin.html#a367942fcb87133bd531708f65270dbdd',1,'BMLTPlugin\cms_get_page_settings_id()'],['../d6/dc9/class_b_m_l_t_u_test_plugin.html#af671789eb3b665f40e664b2d77cf07fb',1,'BMLTUTestPlugin\cms_get_page_settings_id( $in_text, $in_check_mobile=false)']]],
-  ['cms_5fget_5fpost_5fmeta_4',['cms_get_post_meta',['../d6/dc9/class_b_m_l_t_u_test_plugin.html#a600910cb9e11e190b3dfc68eed53c5d4',1,'BMLTUTestPlugin']]],
-  ['cms_5fset_5foption_5',['cms_set_option',['../d5/dc1/class_b_m_l_t_plugin.html#a7b8aaef06ef3c920173f0a1f6c6d6325',1,'BMLTPlugin\cms_set_option()'],['../d6/dc9/class_b_m_l_t_u_test_plugin.html#a7b8aaef06ef3c920173f0a1f6c6d6325',1,'BMLTUTestPlugin\cms_set_option()']]],
-  ['content_5ffilter_6',['content_filter',['../d5/dc1/class_b_m_l_t_plugin.html#abb9c6af3595575613ab48edbdc0c0db8',1,'BMLTPlugin']]]
+  ['cms_5fget_5fadmin_5fnonce_5fhtml_2',['cms_get_admin_nonce_html',['../d5/dc1/class_b_m_l_t_plugin.html#a385e02c6f4235ac47c8d4d45cc0db941',1,'BMLTPlugin']]],
+  ['cms_5fget_5foption_3',['cms_get_option',['../d5/dc1/class_b_m_l_t_plugin.html#a812e94daeb7fb59635668fd8e1219ff2',1,'BMLTPlugin\cms_get_option()'],['../d6/dc9/class_b_m_l_t_u_test_plugin.html#a812e94daeb7fb59635668fd8e1219ff2',1,'BMLTUTestPlugin\cms_get_option()']]],
+  ['cms_5fget_5fpage_5fsettings_5fid_4',['cms_get_page_settings_id',['../d5/dc1/class_b_m_l_t_plugin.html#a367942fcb87133bd531708f65270dbdd',1,'BMLTPlugin\cms_get_page_settings_id()'],['../d6/dc9/class_b_m_l_t_u_test_plugin.html#af671789eb3b665f40e664b2d77cf07fb',1,'BMLTUTestPlugin\cms_get_page_settings_id( $in_text, $in_check_mobile=false)']]],
+  ['cms_5fget_5fpost_5fmeta_5',['cms_get_post_meta',['../d6/dc9/class_b_m_l_t_u_test_plugin.html#a600910cb9e11e190b3dfc68eed53c5d4',1,'BMLTUTestPlugin']]],
+  ['cms_5fset_5foption_6',['cms_set_option',['../d5/dc1/class_b_m_l_t_plugin.html#a7b8aaef06ef3c920173f0a1f6c6d6325',1,'BMLTPlugin\cms_set_option()'],['../d6/dc9/class_b_m_l_t_u_test_plugin.html#a7b8aaef06ef3c920173f0a1f6c6d6325',1,'BMLTUTestPlugin\cms_set_option()']]],
+  ['cms_5fverify_5fadmin_5fnonce_7',['cms_verify_admin_nonce',['../d5/dc1/class_b_m_l_t_plugin.html#a4e01151e8eb88877d28fcc75b49e8e82',1,'BMLTPlugin']]],
+  ['content_5ffilter_8',['content_filter',['../d5/dc1/class_b_m_l_t_plugin.html#abb9c6af3595575613ab48edbdc0c0db8',1,'BMLTPlugin']]]
 ];
